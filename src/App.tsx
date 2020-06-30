799c2f11b2fbd2fe89a9c5ea9a843ddefa9f822b
Ayrton Souza <ayrtonsouza@icloud.com>
2020-06-30 16:37:36 -0300

    global styles development

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import GlobalStyle from './styles/global';
 
-function App() {
-  return <h1>Hello, world!</h1>;
-}
+const App: React.FC = () => (
+  <>
+    <h1>Hello, world!</h1>
+    <GlobalStyle />
+  </>
+);
 
 export default App;
